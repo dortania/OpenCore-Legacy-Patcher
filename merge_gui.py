8cfa90f352690592bd9850c6e6f835f19f3b9afe
Mykola Grymalyuk <48863253+khronokernel@users.noreply.github.com>
2021-06-23 12:34:05 -0600

    Update CI

---
+++ merge_gui.py
@@ -0,0 +1,10 @@
+# Updates build version in OCLP-GUI during CI builds
+# Copyright (C) 2021 Mykola Grymalyuk
+import plistlib
+from pathlib import Path
+from Resources import Constants
+
+app_path = Path.cwd() / Path ("OpenCore Patcher.app/Contents/Info.plist")
+info = plistlib.load(Path(app_path).open("rb"))
+info["CFBundleShortVersionString"] = Constants.Constants().patcher_version
+plistlib.dump(info, Path(app_path).open("wb"), sort_keys=True)
