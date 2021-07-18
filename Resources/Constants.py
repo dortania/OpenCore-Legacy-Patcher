a280e22cdbfb9d80288ce0f2dc32f0a74dc29d3c
Mykola Grymalyuk <48863253+khronokernel@users.noreply.github.com>
2021-07-18 11:40:30 -0600

    Adjust csr formatting

--- Resources/Constants.py
+++ Resources/Constants.py
@@ -290,7 +290,7 @@ class Constants:
     @property
     def innie_path(self):
         return self.payload_kexts_path / Path(f"Misc/Innie-v{self.innie_version}.zip")
-    
+
     @property
     def latebloom_path(self):
         return self.payload_kexts_path / Path(f"Misc/latebloom-v{self.latebloom_version}.zip")
@@ -536,18 +536,21 @@ class Constants:
         return self.payload_apple_kexts_path / Path("Brightness-Control")
 
     csr_values = {
-        "CSR_ALLOW_UNTRUSTED_KEXTS": False,  # 0x1   - Allows Unsigned Kexts           - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_UNRESTRICTED_FS": False,  # 0x2   - File System Access              - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_TASK_FOR_PID": False,  # 0x4   - Unrestricted Debugging          - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_KERNEL_DEBUGGER": False,  # 0x8   - Allow Kernel Debugger           - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_APPLE_INTERNAL": False,  # 0x10  - Set AppleInternal Features      - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_UNRESTRICTED_DTRACE": False,  # 0x20  - Unrestricted DTrace usage       - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_UNRESTRICTED_NVRAM": False,  # 0x40  - Unrestricted NVRAM write        - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_DEVICE_CONFIGURATION": False,  # 0x80  - Allow Device Configuration(?)   - Introduced in El Capitan  # noqa: E241
-        "CSR_ALLOW_ANY_RECOVERY_OS": False,  # 0x100 - Disable BaseSystem Verification - Introduced in Sierra      # noqa: E241
-        "CSR_ALLOW_UNAPPROVED_KEXTS": False,  # 0x200 - Allow Unapproved Kexts          - Introduced in High Sierra # noqa: E241
+        # Source: macOS 11.4 (XNU's csr.h)
+        # https://opensource.apple.com/source/xnu/xnu-7195.121.3/bsd/sys/csr.h.auto.html
+        "CSR_ALLOW_UNTRUSTED_KEXTS": False,  #            0x1   - Allows Unsigned Kexts           - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_UNRESTRICTED_FS": False,  #            0x2   - File System Access              - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_TASK_FOR_PID": False,  #               0x4   - Unrestricted task_for_pid()     - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_KERNEL_DEBUGGER": False,  #            0x8   - Allow Kernel Debugger           - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_APPLE_INTERNAL": False,  #             0x10  - Set AppleInternal Features      - Introduced in El Capitan  # noqa: E241
+        # "CSR_ALLOW_DESTRUCTIVE_DTRACE": False,  #       0x20  - Allow destructive DTrace        - Deprecated                # noqa: E241
+        "CSR_ALLOW_UNRESTRICTED_DTRACE": False,  #        0x20  - Unrestricted DTrace usage       - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_UNRESTRICTED_NVRAM": False,  #         0x40  - Unrestricted NVRAM write        - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_DEVICE_CONFIGURATION": False,  #       0x80  - Allow custom DeviceTree (iOS)   - Introduced in El Capitan  # noqa: E241
+        "CSR_ALLOW_ANY_RECOVERY_OS": False,  #            0x100 - Skip BaseSystem Verification    - Introduced in Sierra      # noqa: E241
+        "CSR_ALLOW_UNAPPROVED_KEXTS": False,  #           0x200 - Allow Unnotarized Kexts         - Introduced in High Sierra # noqa: E241
         "CSR_ALLOW_EXECUTABLE_POLICY_OVERRIDE": False,  # 0x400 - Override Executable Policy      - Introduced in Mojave      # noqa: E241
-        "CSR_ALLOW_UNAUTHENTICATED_ROOT": False,  # 0x800 - Allow Root Volume Mounting      - Introduced in Big Sur     # noqa: E241
+        "CSR_ALLOW_UNAUTHENTICATED_ROOT": False,  #       0x800 - Allow Root Volume Mounting      - Introduced in Big Sur     # noqa: E241
     }
 
     root_patch_sip_mojave = [
