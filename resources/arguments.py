e8d2b4f63e5784e568253707addbd578914e24ae
Mykola Grymalyuk <khronokernel@icloud.com>
2021-10-04 12:47:27 -0600

    Fix on-model building with CLI

--- resources/arguments.py
+++ resources/arguments.py
@@ -95,7 +95,7 @@ If you plan to create the USB for another machine, please select the "Change Mod
 
         # Avoid running the root patcher if we're just building
         if self.args.build:
-            build.BuildOpenCore(settings.custom_model, settings).build_opencore()
+            build.BuildOpenCore(settings.custom_model or settings.computer.real_model, settings).build_opencore()
         elif self.args.patch_sys_vol:
             if self.args.moj_cat_accel:
                 print("- Set Mojave/Catalina root patch configuration")
