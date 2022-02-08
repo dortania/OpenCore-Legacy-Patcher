84bf2a9e184d619fbe6901a41039796cd1997554
Mykola Grymalyuk <khronokernel@icloud.com>
2022-02-08 08:52:25 -0700

    sys_patch_detect.py: Fix backlight detection

--- resources/sys_patch_detect.py
+++ resources/sys_patch_detect.py
@@ -50,7 +50,7 @@ class detect_root_patch:
                     if self.constants.detected_os > non_metal_os:
                         self.nvidia_legacy = True
                         self.amfi_must_disable = True
-                        # self.legacy_keyboard_backlight = self.check_legacy_keyboard_backlight()
+                        self.legacy_keyboard_backlight = self.check_legacy_keyboard_backlight()
                 elif gpu.arch == device_probe.NVIDIA.Archs.Kepler:
                     if self.constants.detected_os > os_data.os_data.big_sur:
                         # Kepler drivers were dropped with Beta 7
