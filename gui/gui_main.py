26bc5fa4c59baa6a455753b8539a3b201194a58c
Mykola Grymalyuk <khronokernel@icloud.com>
2022-04-16 12:40:16 -0600

    gui_main.py: Add non-zip handling

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -1478,9 +1478,14 @@ class wx_python_gui:
                 if not utilities.validate_link(link):
                     print("- Manual backup is missing on Github, no more fallbacks remaining. Quitting")
                     return
-        
-        if utilities.download_file(link, self.constants.installer_pkg_zip_path):
-            subprocess.run(["unzip", "-o", self.constants.installer_pkg_zip_path, "-d", self.constants.installer_pkg_path])
+
+        if link.endswith("zip"):
+            if utilities.download_file(link, self.constants.installer_pkg_zip_path):
+                if Path(self.constants.installer_pkg_path).exists():
+                    os.remove(self.constants.installer_pkg_path)
+                subprocess.run(["unzip", "-o", self.constants.installer_pkg_zip_path, "-d", self.constants.installer_pkg_path])
+        else:
+            utilities.download_file(link, self.constants.installer_pkg_path)
 
     def install_installer_pkg(self, disk):
         disk = disk + "s2" # ESP sits at 1, and we know macOS will have created the main partition at 2
