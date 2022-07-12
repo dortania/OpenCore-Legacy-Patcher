57daf050b245b827fc499044a6890962e0e0e655
Mykola Grymalyuk <khronokernel@icloud.com>
2022-07-12 15:39:53 -0600

    gui_main.py: Adjust prompt

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -1500,7 +1500,8 @@ class wx_python_gui:
             app_major = app_dict['Version'].split(".")[0]
             if float(app_major) > self.constants.os_support:
                 # Throw pop up warning OCLP does not support this OS
-                dlg = wx.MessageDialog(self.frame_modal, f"OpenCore Legacy patcher currently does not support macOS {os_data.os_conversion.convert_kernel_to_marketing_name(os_data.os_conversion.os_to_kernel(app_major))}. We highly recommend you select and older installer.\n\nThe newest version we officially support is macOS {os_data.os_conversion.convert_kernel_to_marketing_name(os_data.os_conversion.os_to_kernel(str(self.constants.os_support)))}", "Unsupported OS", wx.YES_NO | wx.ICON_WARNING)
+                os =  os_data.os_conversion.convert_kernel_to_marketing_name(os_data.os_conversion.os_to_kernel(app_major))
+                dlg = wx.MessageDialog(self.frame_modal, f"OpenCore Legacy patcher currently does not support macOS {os}. We highly recommend you select an older installer.\n\nThe newest version we officially support is macOS {os_data.os_conversion.convert_kernel_to_marketing_name(os_data.os_conversion.os_to_kernel(str(self.constants.os_support)))}\n\nWould you still want to continue downloading macOS {os}?", "Unsupported OS", wx.YES_NO | wx.ICON_WARNING)
                 if dlg.ShowModal() == wx.ID_NO:
                     return
         except ValueError:
