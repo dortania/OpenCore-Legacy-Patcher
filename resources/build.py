d0364d8c91095f6b1d7e6bde00b467913a179698
Mykola Grymalyuk <khronokernel@icloud.com>
2022-09-19 21:07:56 -0600

    build.py: Ensure boot-arg is installed for BCM2046/2070 chipsets

--- resources/build.py
+++ resources/build.py
@@ -812,6 +812,7 @@ class BuildOpenCore:
                 print("- Fixing Legacy Bluetooth for macOS Monterey")
                 self.enable_kext("BlueToolFixup.kext", self.constants.bluetool_version, self.constants.bluetool_path)
                 self.enable_kext("Bluetooth-Spoof.kext", self.constants.btspoof_version, self.constants.btspoof_path)
+                self.config["NVRAM"]["Add"]["7C436110-AB2A-4BBB-A880-FE41995C9F82"]["boot-args"] += " -btlfxallowanyaddr"
             elif self.computer.bluetooth_chipset == "BRCM20702 Hub":
                 # BCM94331 can include either BCM2070 or BRCM20702 v1 Bluetooth chipsets
                 # Note Monterey only natively supports BRCM20702 v2 (found with BCM94360)
@@ -829,6 +830,7 @@ class BuildOpenCore:
             print("- Fixing Legacy Bluetooth for macOS Monterey")
             self.enable_kext("BlueToolFixup.kext", self.constants.bluetool_version, self.constants.bluetool_path)
             if smbios_data.smbios_dictionary[self.model]["Bluetooth Model"] <= bluetooth_data.bluetooth_data.BRCM2070.value:
+                self.config["NVRAM"]["Add"]["7C436110-AB2A-4BBB-A880-FE41995C9F82"]["boot-args"] += " -btlfxallowanyaddr"
                 self.enable_kext("Bluetooth-Spoof.kext", self.constants.btspoof_version, self.constants.btspoof_path)
 
         if self.constants.nvme_boot is True:
