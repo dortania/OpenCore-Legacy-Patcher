59b473a4ce4ffb37d7219470aa764f4ee50da527
Mykola Grymalyuk <khronokernel@icloud.com>
2022-10-29 07:43:47 -0600

    build.py: Add MBP121 as Force Touch capable model

--- resources/build.py
+++ resources/build.py
@@ -813,7 +813,7 @@ class BuildOpenCore:
         if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.skylake.value:
             if self.model.startswith("MacBook"):
                 # These units got force touch early, so ignore them
-                if self.model not in ["MacBookPro11,4", "MacBookPro11,5", "MacBook8,1"]:
+                if self.model not in ["MacBookPro11,4", "MacBookPro11,5", "MacBookPro12,1", "MacBook8,1"]:
                     self.enable_kext("AppleUSBTopCase.kext", self.constants.topcase_version, self.constants.top_case_path)
                     self.get_kext_by_bundle_path("AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCButtons.kext")["Enabled"] = True
                     self.get_kext_by_bundle_path("AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext")["Enabled"] = True
