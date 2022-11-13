235a9985f45d2bf0b0f338f5bc009cea70d061f3
Mykola Grymalyuk <khronokernel@icloud.com>
2022-11-13 15:59:23 -0700

    wired.py: Refactor

--- resources/build/networking/wired.py
+++ resources/build/networking/wired.py
@@ -11,44 +11,56 @@ class build_wired:
         self.computer = self.constants.computer
 
     def build(self):
+        # Check if Ethernet was detected, otherwise fall back to assumptions (mainly for 2011 MacBook Airs and TB Ethernet)
         if not self.constants.custom_model and self.constants.computer.ethernet:
-            for controller in self.constants.computer.ethernet:
-                if isinstance(controller, device_probe.BroadcomEthernet) and controller.chipset == device_probe.BroadcomEthernet.Chipsets.AppleBCM5701Ethernet:
-                    if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.ivy_bridge.value:
-                        # Required due to Big Sur's BCM5701 requiring VT-D support
-                        # Applicable for pre-Ivy Bridge models
-                        if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("CatalinaBCM5701Ethernet.kext")["Enabled"] is False:
-                            support.build_support(self.model, self.constants, self.config).enable_kext("CatalinaBCM5701Ethernet.kext", self.constants.bcm570_version, self.constants.bcm570_path)
-                elif isinstance(controller, device_probe.IntelEthernet):
-                    if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.ivy_bridge.value:
-                        # Apple's IOSkywalkFamily in DriverKit requires VT-D support
-                        # Applicable for pre-Ivy Bridge models
-                        if controller.chipset == device_probe.IntelEthernet.Chipsets.AppleIntelI210Ethernet:
-                            if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("CatalinaIntelI210Ethernet.kext")["Enabled"] is False:
-                                support.build_support(self.model, self.constants, self.config).enable_kext("CatalinaIntelI210Ethernet.kext", self.constants.i210_version, self.constants.i210_path)
-                        elif controller.chipset == device_probe.IntelEthernet.Chipsets.AppleIntel8254XEthernet:
-                            if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("AppleIntel8254XEthernet.kext")["Enabled"] is False:
-                                support.build_support(self.model, self.constants, self.config).enable_kext("AppleIntel8254XEthernet.kext", self.constants.intel_8254x_version, self.constants.intel_8254x_path)
-                        elif controller.chipset == device_probe.IntelEthernet.Chipsets.Intel82574L:
-                            if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("Intel82574L.kext")["Enabled"] is False:
-                                support.build_support(self.model, self.constants, self.config).enable_kext("Intel82574L.kext", self.constants.intel_82574l_version, self.constants.intel_82574l_path)
-                elif isinstance(controller, device_probe.NVIDIAEthernet):
-                    if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("nForceEthernet.kext")["Enabled"] is False:
-                        support.build_support(self.model, self.constants, self.config).enable_kext("nForceEthernet.kext", self.constants.nforce_version, self.constants.nforce_path)
-                elif isinstance(controller, device_probe.Marvell) or  isinstance(controller, device_probe.SysKonnect):
-                    if support.build_support(self.model, self.constants, self.config).get_kext_by_bundle_path("MarvelYukonEthernet.kext")["Enabled"] is False:
-                        support.build_support(self.model, self.constants, self.config).enable_kext("MarvelYukonEthernet.kext", self.constants.marvel_version, self.constants.marvel_path)
+            self.on_model()
         else:
-            if smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Broadcom":
+            self.prebuilt_assumption()
+
+    def on_model(self):
+        # On-model hardware detection
+        for controller in self.constants.computer.ethernet:
+            if isinstance(controller, device_probe.BroadcomEthernet) and controller.chipset == device_probe.BroadcomEthernet.Chipsets.AppleBCM5701Ethernet:
+                if not self.model in smbios_data.smbios_dictionary:
+                    continue
                 if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.ivy_bridge.value:
                     # Required due to Big Sur's BCM5701 requiring VT-D support
                     # Applicable for pre-Ivy Bridge models
                     support.build_support(self.model, self.constants, self.config).enable_kext("CatalinaBCM5701Ethernet.kext", self.constants.bcm570_version, self.constants.bcm570_path)
-            elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Nvidia":
+            elif isinstance(controller, device_probe.IntelEthernet):
+                if not self.model in smbios_data.smbios_dictionary:
+                    continue
+                if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.ivy_bridge.value:
+                    # Apple's IOSkywalkFamily in DriverKit requires VT-D support
+                    # Applicable for pre-Ivy Bridge models
+                    if controller.chipset == device_probe.IntelEthernet.Chipsets.AppleIntelI210Ethernet:
+                        support.build_support(self.model, self.constants, self.config).enable_kext("CatalinaIntelI210Ethernet.kext", self.constants.i210_version, self.constants.i210_path)
+                    elif controller.chipset == device_probe.IntelEthernet.Chipsets.AppleIntel8254XEthernet:
+                        support.build_support(self.model, self.constants, self.config).enable_kext("AppleIntel8254XEthernet.kext", self.constants.intel_8254x_version, self.constants.intel_8254x_path)
+                    elif controller.chipset == device_probe.IntelEthernet.Chipsets.Intel82574L:
+                        support.build_support(self.model, self.constants, self.config).enable_kext("Intel82574L.kext", self.constants.intel_82574l_version, self.constants.intel_82574l_path)
+            elif isinstance(controller, device_probe.NVIDIAEthernet):
                 support.build_support(self.model, self.constants, self.config).enable_kext("nForceEthernet.kext", self.constants.nforce_version, self.constants.nforce_path)
-            elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Marvell":
+            elif isinstance(controller, device_probe.Marvell) or isinstance(controller, device_probe.SysKonnect):
                 support.build_support(self.model, self.constants, self.config).enable_kext("MarvelYukonEthernet.kext", self.constants.marvel_version, self.constants.marvel_path)
-            elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Intel 80003ES2LAN":
-                support.build_support(self.model, self.constants, self.config).enable_kext("AppleIntel8254XEthernet.kext", self.constants.intel_8254x_version, self.constants.intel_8254x_path)
-            elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Intel 82574L":
-                support.build_support(self.model, self.constants, self.config).enable_kext("Intel82574L.kext", self.constants.intel_82574l_version, self.constants.intel_82574l_path)
+
+    def prebuilt_assumption(self):
+        # Stock hardware assumptions
+        if not self.model in smbios_data.smbios_dictionary:
+            return
+        if not "Ethernet Chipset" in smbios_data.smbios_dictionary[self.model]:
+            return
+
+        if smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Broadcom":
+            if smbios_data.smbios_dictionary[self.model]["CPU Generation"] < cpu_data.cpu_data.ivy_bridge.value:
+                # Required due to Big Sur's BCM5701 requiring VT-D support
+                # Applicable for pre-Ivy Bridge models
+                support.build_support(self.model, self.constants, self.config).enable_kext("CatalinaBCM5701Ethernet.kext", self.constants.bcm570_version, self.constants.bcm570_path)
+        elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Nvidia":
+            support.build_support(self.model, self.constants, self.config).enable_kext("nForceEthernet.kext", self.constants.nforce_version, self.constants.nforce_path)
+        elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Marvell":
+            support.build_support(self.model, self.constants, self.config).enable_kext("MarvelYukonEthernet.kext", self.constants.marvel_version, self.constants.marvel_path)
+        elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Intel 80003ES2LAN":
+            support.build_support(self.model, self.constants, self.config).enable_kext("AppleIntel8254XEthernet.kext", self.constants.intel_8254x_version, self.constants.intel_8254x_path)
+        elif smbios_data.smbios_dictionary[self.model]["Ethernet Chipset"] == "Intel 82574L":
+            support.build_support(self.model, self.constants, self.config).enable_kext("Intel82574L.kext", self.constants.intel_82574l_version, self.constants.intel_82574l_path)
