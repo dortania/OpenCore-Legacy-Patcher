d7ecdf3871732166537e724fce145ab16786d2e2
Mykola Grymalyuk <khronokernel@icloud.com>
2022-12-25 13:58:08 -0700

    sys_patch: Adjust more indenation logic

--- resources/sys_patch/sys_patch_auto.py
+++ resources/sys_patch/sys_patch_auto.py
@@ -24,90 +24,96 @@ class AutomaticSysPatch:
 
     def start_auto_patch(self):
         print("- Starting Automatic Patching")
-        if self.constants.wxpython_variant is True:
-            if utilities.check_seal() is True:
-                print("- Detected Snapshot seal intact, detecting patches")
-                patches = sys_patch_detect.detect_root_patch(self.constants.computer.real_model, self.constants).detect_patch_set()
-                if not any(not patch.startswith("Settings") and not patch.startswith("Validation") and patches[patch] is True for patch in patches):
-                    patches = []
-                if patches:
-                    print("- Detected applicable patches, determining whether possible to patch")
-                    if patches["Validation: Patching Possible"] is True:
-                        print("- Determined patching is possible, checking for OCLP updates")
-                        patch_string = ""
-                        for patch in patches:
-                            if patches[patch] is True and not patch.startswith("Settings") and not patch.startswith("Validation"):
-                                patch_string += f"- {patch}\n"
-                        # Check for updates
-                        dict = updates.check_binary_updates(self.constants).check_binary_updates()
-                        if not dict:
-                            print("- No new binaries found on Github, proceeding with patching")
-                            if self.constants.launcher_script is None:
-                                args_string = f"'{self.constants.launcher_binary}' --gui_patch"
-                            else:
-                                args_string = f"{self.constants.launcher_binary} {self.constants.launcher_script} --gui_patch"
-
-                            warning_str = ""
-                            if utilities.verify_network_connection("https://api.github.com/repos/dortania/OpenCore-Legacy-Patcher/releases/latest") is False:
-                                warning_str = f"""\n\nWARNING: We're unable to verify whether there are any new releases of OpenCore Legacy Patcher on Github. Be aware that you may be using an outdated version for this OS. If you're unsure, verify on Github that OpenCore Legacy Patcher {self.constants.patcher_version} is the latest official release"""
-
-                            args = [
-                                "osascript",
-                                "-e",
-                                f"""display dialog "OpenCore Legacy Patcher has detected you're running without Root Patches, and would like to install them.\n\nmacOS wipes all root patches during OS installs and updates, so they need to be reinstalled.\n\nFollowing Patches have been detected for your system: \n{patch_string}\nWould you like to apply these patches?{warning_str}" """
-                                f'with icon POSIX file "{self.constants.app_icon_path}"',
-                            ]
-                            output = subprocess.run(
-                                args,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT
-                            )
-                            if output.returncode == 0:
-                                args = [
-                                    "osascript",
-                                    "-e",
-                                    f'''do shell script "{args_string}"'''
-                                    f' with prompt "OpenCore Legacy Patcher would like to patch your root volume"'
-                                    " with administrator privileges"
-                                    " without altering line endings"
-                                ]
-                                subprocess.run(
-                                    args,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.STDOUT
-                                )
-                        else:
-                            for key in dict:
-                                version = dict[key]["Version"]
-                                github_link = dict[key]["Github Link"]
-                            print(f"- Found new version: {version}")
-
-                            # launch osascript to ask user if they want to apply the update
-                            # if yes, open the link in the default browser
-                            # we never want to run the root patcher if there are updates available
-                            args = [
-                                "osascript",
-                                "-e",
-                                f"""display dialog "OpenCore Legacy Patcher has detected you're running without Root Patches, and would like to install them.\n\nHowever we've detected a new version of OCLP on Github. Would you like to view this?\n\nCurrent Version: {self.constants.patcher_version}\nLatest Version: {version}\n\nNote: After downloading the latest OCLP version, open the app and run the 'Post Install Root Patcher' from the main menu." """
-                                f'with icon POSIX file "{self.constants.app_icon_path}"',
-                            ]
-                            output = subprocess.run(
-                                args,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT
-                            )
-                            if output.returncode == 0:
-                                webbrowser.open(github_link)
-                    else:
-                        print("- Cannot run patching")
-                else:
-                    print("- No patches detected")
-                    self.determine_if_boot_matches()
-            else:
-                print("- Detected Snapshot seal not intact, skipping")
-                self.determine_if_boot_matches(self.constants)
-        else:
+        if self.constants.wxpython_variant is False:
             print("- Auto Patch option is not supported on TUI, please use GUI")
+            return
+
+        if utilities.check_seal() is True:
+            print("- Detected Snapshot seal intact, detecting patches")
+            patches = sys_patch_detect.detect_root_patch(self.constants.computer.real_model, self.constants).detect_patch_set()
+            if not any(not patch.startswith("Settings") and not patch.startswith("Validation") and patches[patch] is True for patch in patches):
+                patches = []
+            if patches:
+                print("- Detected applicable patches, determining whether possible to patch")
+                if patches["Validation: Patching Possible"] is False:
+                    print("- Cannot run patching")
+                    return
+
+                print("- Determined patching is possible, checking for OCLP updates")
+                patch_string = ""
+                for patch in patches:
+                    if patches[patch] is True and not patch.startswith("Settings") and not patch.startswith("Validation"):
+                        patch_string += f"- {patch}\n"
+                # Check for updates
+                dict = updates.check_binary_updates(self.constants).check_binary_updates()
+                if not dict:
+                    print("- No new binaries found on Github, proceeding with patching")
+                    if self.constants.launcher_script is None:
+                        args_string = f"'{self.constants.launcher_binary}' --gui_patch"
+                    else:
+                        args_string = f"{self.constants.launcher_binary} {self.constants.launcher_script} --gui_patch"
+
+                    warning_str = ""
+                    if utilities.verify_network_connection("https://api.github.com/repos/dortania/OpenCore-Legacy-Patcher/releases/latest") is False:
+                        warning_str = f"""\n\nWARNING: We're unable to verify whether there are any new releases of OpenCore Legacy Patcher on Github. Be aware that you may be using an outdated version for this OS. If you're unsure, verify on Github that OpenCore Legacy Patcher {self.constants.patcher_version} is the latest official release"""
+
+                    args = [
+                        "osascript",
+                        "-e",
+                        f"""display dialog "OpenCore Legacy Patcher has detected you're running without Root Patches, and would like to install them.\n\nmacOS wipes all root patches during OS installs and updates, so they need to be reinstalled.\n\nFollowing Patches have been detected for your system: \n{patch_string}\nWould you like to apply these patches?{warning_str}" """
+                        f'with icon POSIX file "{self.constants.app_icon_path}"',
+                    ]
+                    output = subprocess.run(
+                        args,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT
+                    )
+                    if output.returncode == 0:
+                        args = [
+                            "osascript",
+                            "-e",
+                            f'''do shell script "{args_string}"'''
+                            f' with prompt "OpenCore Legacy Patcher would like to patch your root volume"'
+                            " with administrator privileges"
+                            " without altering line endings"
+                        ]
+                        subprocess.run(
+                            args,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT
+                        )
+                    return
+                else:
+                    for key in dict:
+                        version = dict[key]["Version"]
+                        github_link = dict[key]["Github Link"]
+                    print(f"- Found new version: {version}")
+
+                    # launch osascript to ask user if they want to apply the update
+                    # if yes, open the link in the default browser
+                    # we never want to run the root patcher if there are updates available
+                    args = [
+                        "osascript",
+                        "-e",
+                        f"""display dialog "OpenCore Legacy Patcher has detected you're running without Root Patches, and would like to install them.\n\nHowever we've detected a new version of OCLP on Github. Would you like to view this?\n\nCurrent Version: {self.constants.patcher_version}\nLatest Version: {version}\n\nNote: After downloading the latest OCLP version, open the app and run the 'Post Install Root Patcher' from the main menu." """
+                        f'with icon POSIX file "{self.constants.app_icon_path}"',
+                    ]
+                    output = subprocess.run(
+                        args,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT
+                    )
+                    if output.returncode == 0:
+                        webbrowser.open(github_link)
+
+                    return
+            else:
+                print("- No patches detected")
+        else:
+            print("- Detected Snapshot seal not intact, skipping")
+
+        self.determine_if_boot_matches()
+
 
     def determine_if_boot_matches(self):
         # Goal of this function is to determine whether the user
@@ -118,61 +124,65 @@ class AutomaticSysPatch:
         # and ask if they want to install to install to disk
 
         print("- Determining if macOS drive matches boot drive")
-
         should_notify = global_settings.global_settings().read_property("AutoPatch_Notify_Mismatched_Disks")
         if should_notify is False:
             print("- Skipping due to user preference")
-        elif self.constants.host_is_hackintosh is True:
+            return
+        if self.constants.host_is_hackintosh is True:
             print("- Skipping due to hackintosh")
-        else:
-            if self.constants.booted_oc_disk:
-                root_disk = self.constants.booted_oc_disk.strip("disk")
-                root_disk = "disk" + root_disk.split("s")[0]
+            return
+        if not self.constants.booted_oc_disk:
+            print("- Failed to find disk OpenCore launched from")
+            return
 
-                print(f"  - Boot Drive: {self.constants.booted_oc_disk} ({root_disk})")
-                macOS_disk = utilities.get_disk_path()
-                print(f"  - macOS Drive: {macOS_disk}")
-                physical_stores = utilities.find_apfs_physical_volume(macOS_disk)
-                print(f"  - APFS Physical Stores: {physical_stores}")
+        root_disk = self.constants.booted_oc_disk.strip("disk")
+        root_disk = "disk" + root_disk.split("s")[0]
 
-                disk_match = False
-                for disk in physical_stores:
-                    if root_disk in disk:
-                        print(f"- Boot drive matches macOS drive ({disk})")
-                        disk_match = True
-                        break
+        print(f"  - Boot Drive: {self.constants.booted_oc_disk} ({root_disk})")
+        macOS_disk = utilities.get_disk_path()
+        print(f"  - macOS Drive: {macOS_disk}")
+        physical_stores = utilities.find_apfs_physical_volume(macOS_disk)
+        print(f"  - APFS Physical Stores: {physical_stores}")
 
-                if disk_match is False:
-                    # Check if OpenCore is on a USB drive
-                    print("- Boot Drive does not match macOS drive, checking if OpenCore is on a USB drive")
+        disk_match = False
+        for disk in physical_stores:
+            if root_disk in disk:
+                print(f"- Boot drive matches macOS drive ({disk})")
+                disk_match = True
+                break
 
-                    disk_info = plistlib.loads(subprocess.run(["diskutil", "info", "-plist", root_disk], stdout=subprocess.PIPE).stdout)
-                    try:
-                        if disk_info["Ejectable"] is True:
-                            print("- Boot Disk is ejectable, prompting user to install to internal")
+        if disk_match is True:
+            return
 
-                            args = [
-                                "osascript",
-                                "-e",
-                                f"""display dialog "OpenCore Legacy Patcher has detected that you are booting OpenCore from an USB or External drive.\n\nIf you would like to boot your Mac normally without a USB drive plugged in, you can install OpenCore to the internal hard drive.\n\nWould you like to launch OpenCore Legacy Patcher and install to disk?" """
-                                f'with icon POSIX file "{self.constants.app_icon_path}"',
-                            ]
-                            output = subprocess.run(
-                                args,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT
-                            )
-                            if output.returncode == 0:
-                                print("- Launching GUI's Build/Install menu")
-                                self.constants.start_build_install = True
-                                gui_main.wx_python_gui(self.constants).main_menu(None)
-                        else:
-                            print("- Boot Disk is not removable, skipping prompt")
-                    except KeyError:
-                        print("- Unable to determine if boot disk is removable, skipping prompt")
+        # Check if OpenCore is on a USB drive
+        print("- Boot Drive does not match macOS drive, checking if OpenCore is on a USB drive")
 
-            else:
-                print("- Failed to find disk OpenCore launched from")
+        disk_info = plistlib.loads(subprocess.run(["diskutil", "info", "-plist", root_disk], stdout=subprocess.PIPE).stdout)
+        try:
+            if disk_info["Ejectable"] is False:
+                print("- Boot Disk is not removable, skipping prompt")
+                return
+
+            print("- Boot Disk is ejectable, prompting user to install to internal")
+
+            args = [
+                "osascript",
+                "-e",
+                f"""display dialog "OpenCore Legacy Patcher has detected that you are booting OpenCore from an USB or External drive.\n\nIf you would like to boot your Mac normally without a USB drive plugged in, you can install OpenCore to the internal hard drive.\n\nWould you like to launch OpenCore Legacy Patcher and install to disk?" """
+                f'with icon POSIX file "{self.constants.app_icon_path}"',
+            ]
+            output = subprocess.run(
+                args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT
+            )
+            if output.returncode == 0:
+                print("- Launching GUI's Build/Install menu")
+                self.constants.start_build_install = True
+                gui_main.wx_python_gui(self.constants).main_menu(None)
+
+        except KeyError:
+            print("- Unable to determine if boot disk is removable, skipping prompt")
 
 
     def install_auto_patcher_launch_agent(self):
