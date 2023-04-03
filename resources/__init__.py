38025d7f2a7d3e899739fdb36b4a1c4074282e7c
Mykola Grymalyuk <khronokernel@icloud.com>
2023-04-03 10:48:24 -0600

    Remove unneeded init.py

--- resources/__init__.py
+++
@@ -1,5 +0,0 @@
-# Credit: CorpNewt
-from os.path import dirname, basename, isfile
-import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
