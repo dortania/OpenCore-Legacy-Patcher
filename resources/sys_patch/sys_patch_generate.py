b87737f55e1541f3a4379263698c9cc30f3b3be7
Jazzzny <jazzzny225@gmail.com>
2023-04-19 16:04:53 -0400

    Create modified build without ColorSync downgrade

--- resources/sys_patch/sys_patch_generate.py
+++ resources/sys_patch/sys_patch_generate.py
@@ -53,8 +53,7 @@ class GenerateRootPatchSets:
             required_patches.update({"WebKit Monterey Common": all_hardware_patchset["Graphics"]["WebKit Monterey Common"]})
             required_patches.update({"Intel Sandy Bridge": all_hardware_patchset["Graphics"]["Intel Sandy Bridge"]})
             # Patchset breaks Display Profiles, don't install if primary GPU is AMD
-            if self.constants.computer.real_model not in ["Macmini5,2", "iMac12,1", "iMac12,2"]:
-                required_patches.update({"Non-Metal ColorSync Workaround": all_hardware_patchset["Graphics"]["Non-Metal ColorSync Workaround"]})
+
 
         if self.hardware_details["Graphics: Intel Ivy Bridge"] is True:
             required_patches.update({"Metal 3802 Common": all_hardware_patchset["Graphics"]["Metal 3802 Common"]})
