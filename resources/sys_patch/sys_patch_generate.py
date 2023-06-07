9a483181a1288567daa3af4ce6d12ad855882764
Mykola Grymalyuk <khronokernel@icloud.com>
2023-06-07 12:12:22 -0600

    sys_patch_generate.py: Add support reference for 31001

--- resources/sys_patch/sys_patch_generate.py
+++ resources/sys_patch/sys_patch_generate.py
@@ -65,6 +65,7 @@ class GenerateRootPatchSets:
             required_patches.update({"WebKit Monterey Common": all_hardware_patchset["Graphics"]["WebKit Monterey Common"]})
             required_patches.update({"Intel Ivy Bridge": all_hardware_patchset["Graphics"]["Intel Ivy Bridge"]})
             required_patches.update({"Sonoma Intel Extended": all_hardware_patchset["Graphics"]["Sonoma Intel Extended"]})
+            required_patches.update({"Sonoma 31001 Extended": all_hardware_patchset["Graphics"]["Sonoma 31001 Extended"]})
 
         if self.hardware_details["Graphics: Intel Haswell"] is True:
             required_patches.update({"Metal 3802 Common": all_hardware_patchset["Graphics"]["Metal 3802 Common"]})
@@ -73,18 +74,21 @@ class GenerateRootPatchSets:
             required_patches.update({"Monterey OpenCL": all_hardware_patchset["Graphics"]["Monterey OpenCL"]})
             required_patches.update({"Intel Haswell": all_hardware_patchset["Graphics"]["Intel Haswell"]})
             required_patches.update({"Sonoma Intel Extended": all_hardware_patchset["Graphics"]["Sonoma Intel Extended"]})
+            required_patches.update({"Sonoma 31001 Extended": all_hardware_patchset["Graphics"]["Sonoma 31001 Extended"]})
 
         if self.hardware_details["Graphics: Intel Broadwell"] is True:
             required_patches.update({"Monterey GVA": all_hardware_patchset["Graphics"]["Monterey GVA"]})
             required_patches.update({"Monterey OpenCL": all_hardware_patchset["Graphics"]["Monterey OpenCL"]})
             required_patches.update({"Intel Broadwell": all_hardware_patchset["Graphics"]["Intel Broadwell"]})
             required_patches.update({"Sonoma Intel Extended": all_hardware_patchset["Graphics"]["Sonoma Intel Extended"]})
+            required_patches.update({"Sonoma 31001 Extended": all_hardware_patchset["Graphics"]["Sonoma 31001 Extended"]})
 
         if self.hardware_details["Graphics: Intel Skylake"] is True:
             required_patches.update({"Monterey GVA": all_hardware_patchset["Graphics"]["Monterey GVA"]})
             required_patches.update({"Monterey OpenCL": all_hardware_patchset["Graphics"]["Monterey OpenCL"]})
             required_patches.update({"Intel Skylake": all_hardware_patchset["Graphics"]["Intel Skylake"]})
             required_patches.update({"Sonoma Intel Extended": all_hardware_patchset["Graphics"]["Sonoma Intel Extended"]})
+            required_patches.update({"Sonoma 31001 Extended": all_hardware_patchset["Graphics"]["Sonoma 31001 Extended"]})
 
         if self.hardware_details["Graphics: Nvidia Tesla"] is True:
             required_patches.update({"Non-Metal Common": all_hardware_patchset["Graphics"]["Non-Metal Common"]})
--- resources/wx_gui/gui_update.py
+++ resources/wx_gui/gui_update.py
@@ -179,7 +179,6 @@ class UpdateFrame(wx.Frame):
         sys.exit(0)
 
 
-
     def _extract_update(self) -> None:
         """
         Extracts the update
