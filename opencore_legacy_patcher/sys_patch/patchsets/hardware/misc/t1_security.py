5a1fa12e5ba70a244e662b0910c3fb39421b50f7
Mykola Grymalyuk <khronokernel@icloud.com>
2024-09-05 09:09:48 -0600

    t1_security.py: Temporarily disable Touch ID patch

--- opencore_legacy_patcher/sys_patch/patchsets/hardware/misc/t1_security.py
+++ opencore_legacy_patcher/sys_patch/patchsets/hardware/misc/t1_security.py
@@ -51,13 +51,13 @@ class T1SecurityChip(BaseHardware):
         """
         if self.native_os() is True:
             return {}
-        
+
         return {
             "T1 Security Chip": {
                 PatchType.INSTALL_SYSTEM_VOLUME: {
                     "/System/Library/Frameworks/LocalAuthentication.framework/Support": {
                         "SharedUtils.framework": f"13.6-{self._xnu_major}",  # Required for Password Authentication (SharedUtils.framework)
-                        **({ "MechanismPlugins": "15.0 Beta 4" } if self._xnu_major >= os_data.sequoia else {}), # Required to add a TouchID fingerprint
+                        #**({ "MechanismPlugins": "15.0 Beta 4" } if self._xnu_major >= os_data.sequoia else {}), # Required to add a TouchID fingerprint
                     },
                     "/System/Library/PrivateFrameworks": {
                         "EmbeddedOSInstall.framework": "13.6",  # Required for biometrickitd
